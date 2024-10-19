0138720bf405bb4d277882cc0031740312031cb6
Elmar van Riet <elmarvr@Elmars-MacBook-Pro.local>
2024-10-19 02:22:38 +0200

    Remove popover

--- app/components/ui/select/select-content.vue
+++ app/components/ui/select/select-content.vue
@@ -4,7 +4,13 @@ import {
   type SelectContentEmits,
   type SelectContentProps,
 } from "radix-vue";
-import { popoverVariants } from "../popover/popover-content.vue";
+
+const popoverVariants = compose(
+  focusRing,
+  cva({
+    base: "bg-popover rounded border text-popover-foreground p-4 z-20 ui-state-open:animate-in ui-state-open:fade-in ui-state-open:zoom-in-95 ui-state-closed:animate-out ui-state-closed:fade-out ui-state-closed:zoom-out-95 ui-side-bottom:slide-in-from-top-2 ui-side-top:slide-in-from-bottom-2",
+  })
+);
 
 defineOptions({
   inheritAttrs: false,
